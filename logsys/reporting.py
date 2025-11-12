10a940d2f9bedcce4e981f93bae3bccefdd04d6d
andy <andyy@zyt.com>
2025-11-13 01:19:06 +0800

    111111

--- logsys/reporting.py
+++ logsys/reporting.py
@@ -3,3 +3,4 @@ from .db import Database
 def top_keys(db: Database, run_id:int, limit=20):
     sql='''\nSELECT template_id, mod, smod, classification, level, thread_id, line_count FROM LOG_MATCH_SUMMARY WHERE run_id=? ORDER BY line_count DESC LIMIT ?\n'''
     return db.query(sql, (run_id, limit))
+
